c7111ca05305e27edc09d63225e4ecbd83708838
Hemery Fred <fred.hemery@univ-artois.fr>
2016-09-11 19:17:28 +0200

    lecture5

---
+++ Lecture5/app.js
@@ -0,0 +1,13 @@
+/**
+ * Created by hemery on 11/09/2016.
+ */
+(function () {
+    'use strict';
+
+    angular.module('myFirstApp', [])
+
+        .controller('MyFirstController', function () {
+
+        });
+
+})();
